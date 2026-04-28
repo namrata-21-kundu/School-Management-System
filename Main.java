061ab13b9480b4441e9c548881622e94f6849227
Namrata Kundu <namratakundu22work@gmail.com>
2026-04-28 19:27:25 +0530

    Main.java skeleton add

--- Main.java
+++ Main.java
@@ -2,6 +2,29 @@ import java.util.* ;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        while()
+        EventManager manager = new EventManager();
+
+        while(true){
+            System.out.println("1. Add Event");
+            System.out.println("2. View Events");
+            System.out.println("3. Register Participant");
+            System.out.println("4. Exit");
+
+            int choice = sc.nextInt();
+
+            switch(choice){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("Thanks for visiting Event Management System. Exiting ---");
+                    break;
+                default:
+                    System.out.println("Wrong choice");
+            }
+        }
     }
 }
