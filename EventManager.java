b6b34c61b46eedea8ed673c76d3e3df07c086de0
Namrata Kundu <namratakundu22work@gmail.com>
2026-04-28 20:10:42 +0530

    updated EventManager

--- EventManager.java
+++ EventManager.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class EventManager {
+    private ArrayList<Event> events;
+    private ArrayList<Participant> participants;
+    private ArrayList<Organiser> organisers;
+    private ArrayList<Ticket> tickets;
+    
+    //Constructor 
+    public EventManager(){
+        events = new ArrayList<>();
+        participants = new ArrayList<>();
+        organisers = new ArrayList<>();
+        tickets = new ArrayList<>();
+    }
+
+    //Event methods
     
 }
