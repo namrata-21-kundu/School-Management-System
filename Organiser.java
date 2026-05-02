9c01ccff3ebb267a3eb1fae57aa4dd5c3c1e83d9
Namrata Kundu <namratakundu22work@gmail.com>
2026-05-03 02:35:33 +0530

    add Organiser.java and update Participant variable declaration remove

--- Organiser.java
+++ Organiser.java
@@ -1,3 +1,13 @@
-public class Organiser {
+public class Organiser extends Person {
+    private String department;
     
+    public Organiser(int id, String name, String phone, String department){
+        super(id, name, phone);
+        this.department = department;
+    }
+
+    public void displayOrganiser(){
+        displayPerson();
+        System.out.println("Department: " + department);
+    }
 }
--- Participant.java
+++ Participant.java
@@ -1,5 +1,4 @@
 public class Participant extends Person {
-    int id, String name, String phone;
     public Participant(int id, String name, String phone){
         super(id, name, phone);
     }
