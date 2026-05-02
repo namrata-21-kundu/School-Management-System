effcce3c0d0b5fabc63d4a11ef6e75f5c69e1411
Namrata Kundu <namratakundu22work@gmail.com>
2026-05-03 02:29:15 +0530

    add Participant.java and update

--- Participant.java
+++ Participant.java
@@ -1,3 +1,10 @@
-public class Participant {
-    
+public class Participant extends Person {
+    int id, String name, String phone;
+    public Participant(int id, String name, String phone){
+        super(id, name, phone);
+    }
+
+    public void displayParticipant(){
+        displayPerson();
+    }
 }
