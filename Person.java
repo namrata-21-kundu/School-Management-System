107ed43c3536060be16df3ce659b30915c5672cb
Namrata Kundu <namratakundu22work@gmail.com>
2026-05-03 02:26:52 +0530

    add Person.java and update

--- Person.java
+++ Person.java
@@ -1,3 +1,17 @@
 public class Person {
-    
+    protected int id;
+    protected String name;
+    protected String phone;
+
+    public Person(int id, String name, String phone){
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public void displayPerson(){
+        System.out.println("ID: "+ id);
+        System.out.println("Name: "+name);
+        System.out.println("Phone No: "+phone);
+    }
 }
