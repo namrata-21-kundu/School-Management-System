3301197f5241ea10714f2ef44de5f0277fc5f3dd
Namrata Kundu <namratakundu22work@gmail.com>
2026-06-25 00:23:01 +0530

    add: Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,19 @@
+public class Teacher extends Person {
+    private String subject;
+    private double salary;
+
+    public Teacher(int id, String name, int age, String subject, double salary){
+        super(id,name,age);
+        this.subject = subject;
+        this.salary = salary;
+    }
+
+    @Override
+    public void displayInfo(){
+        System.out.println("---Teacher Details---");
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Subject: "+subject);
+        System.out.println("Salary: "+salary);
+    }
+}
